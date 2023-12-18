4b9e1f56e7526c85d59bc94b4a5c5eded6baa914
CalinCretu <calincretu1412@gmail.com>
2023-12-18 17:30:09 +0100

    ADD - seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -229,11 +229,13 @@ class ComicSeeder extends Seeder
 
     foreach ($comics as $comic) {
       $new_comic = new Comic();
+      $price = str_replace('$', '', trim($comic['price']));
+      $price_float = floatval($price);
 
       $new_comic->title = $comic['title'];
       $new_comic->description = $comic['description'];
       $new_comic->thumb = $comic['thumb'];
-      $new_comic->price = $comic['price'];
+      $new_comic->price = $price_float;
       $new_comic->series = $comic['series'];
       $new_comic->sale_date = $comic['sale_date'];
       $new_comic->type = $comic['type'];
